7ab923c3b02fa7ac8d0ba0af7f7f534b41f27411
Waleed Ahmad <waleedmian194@gmail.com>
2026-07-13 16:52:19 +0500

    Update urls.py

--- backend/research/urls.py
+++ backend/research/urls.py
@@ -1,13 +1,10 @@
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .viewset import ResearchReportViewSet
-from .views import debug_migrate
 
 router = DefaultRouter()
 router.register(r'reports', ResearchReportViewSet, basename='report')
 
 urlpatterns = [
-    path('debug-migrate/', debug_migrate),
     path('', include(router.urls)),
 ]
